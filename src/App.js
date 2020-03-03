1c7ad7eb66bba716667e362210f69006b5cf5364
lawrayette <milhadasdepapel@gmail.com>
2020-03-03 16:23:47 -0600

    ejercicio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from "react";
+import "./App.css";
+import Header from "./header";
+import Movie from "./movie";
+import Search from "./search";
 
-function App() {
-  return (
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; // you should replace this with yours
+
+// inicializa el estado...array de peliculas vacio y un mensaje de error nulo, lo que significa que no hay error a la hora de la carga
+// inicial
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+// Reducers specify how the application's state changes in response to actions sent to the store. 
+//Remember that actions only describe what happened, but don't describe how the application's state changes.
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {    
+        fetch(MOVIE_API_URL)
+            .then(response => response.json())
+            .then(jsonResponse => {
+        
+            dispatch({
+                type: "SEARCH_MOVIES_SUCCESS",
+                payload: jsonResponse.Search
+        	});
+      	});
+  	}, []);
+
+    const search = searchValue => {
+    	dispatch({
+      	type: "SEARCH_MOVIES_REQUEST"
+    	});
+	
+        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      	.then(response => response.json())
+      	.then(jsonResponse => {
+        	if (jsonResponse.Response === "True") {
+          	dispatch({
+                type: "SEARCH_MOVIES_SUCCESS",
+                payload: jsonResponse.Search
+          	});
+        	} else {
+          	dispatch({
+                type: "SEARCH_MOVIES_FAILURE",
+                error: jsonResponse.Error
+          	});
+          }
+      	});
+	  };
+
+    const { movies, errorMessage, loading } = state;
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading... </span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
